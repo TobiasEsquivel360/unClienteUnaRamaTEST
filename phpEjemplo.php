0e0cba236edb4cf0a184d0d4e24c2f0dc9afaa6b
TobiasEsquivel360 <tesquivel@360strategicrisk.com>
2025-06-25 13:09:10 -0300

    commit prueba 3

--- phpEjemplo.php
+++ phpEjemplo.php
@@ -3,6 +3,7 @@
     echo "Cambio en el core";
     echo "Cambio compartido en todos los sir";
 
-    echo "Cambio compartido antes de powershell 2"
+    echo "Cambio compartido antes de powershell 2";
+    echo "Cambio compartido antes de powershell 3";
 
 ?>
