235d41921477c712f82b51043a508ec26fac2dd7
TobiasEsquivel360 <tesquivel@360strategicrisk.com>
2025-06-27 13:30:19 -0300

    agregados archivos faltantes

---
+++ phpEjemplo - copia (6).php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Cambio en el core";
+    echo "Cambio compartido en todos los sir";
+
+    echo "Cambio compartido antes de powershell 2";
+    echo "Cambio compartido antes de powershell 3";
+    echo "Cambio compartido antes de powershell 4";
+    echo "Cambio compartido antes de powershell 5";
+    echo "Cambio compartido antes de powershell 6";
+
+?>
